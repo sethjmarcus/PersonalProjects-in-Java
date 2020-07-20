02f0fb8d964cfb965f5ba4fe8cbe37606770ec76
sethjmarcus <62255036+sethjmarcus@users.noreply.github.com>
2020-07-20 15:37:17 -0400

    Create GraphColoring.java

---
+++ GraphColoring/GraphColoring.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class GrpahColoring {
+
+   public static void main(String[] args) throws Exception{
+      final int numberOfVertecies = 10;
+      Graph<Integer> g = new Graph<Integer>(false);
+      //Create a graph of random data
+      for (int i = 0; i < numberOfVertecies; i++) {
+         Vertex<Integer> v = new Vertex<Integer>("" + i, i);
+         g.insertVertex(v);
+      }
+      for (int i = 0; i < numberOfVertecies; i++) {
+         int x = (int) (Math.random() * numberOfVertecies);
+         int y = (int) (Math.random() * numberOfVertecies);
+         if (x == y) i--;
+         else {
+            try {
+               g.insertEdge(g.findVertex("" + x), g.findVertex("" + y));
+            } catch (Exception e) {}
+         }
+      }
+      System.out.println("Graph with "+numberOfVertecies+" vertices and " + g.numEdges() + " edges.\n");
+      
+      //printGraph(g);
+      
+      System.out.println("A coloring of the Graph is:");
+      HashMap<Vertex<Integer>, Integer> graphColoring = g.coloring();
+      System.out.println(graphColoring);
+
+   }
+   
+   public static void printGraph(Graph<Integer> g) {
+      for(Vertex<Integer> node : g.vertices) {
+         System.out.print(node.data + ":\t");
+         for(Vertex<Integer> j : node.adjacent.keySet()) {
+            System.out.print(j.data + " ");
+         }
+         System.out.println();
+      }
+   }
+   
+}
